06a857794e67ae89d316c692a7ef20a6bc15809b
bss6720 <22442453+bss6720@users.noreply.github.com>
2021-12-28 15:42:41 +0900

    Initial commit

---
+++ 3-Greedy/3-2_BigNumberRule.py
@@ -0,0 +1,10 @@
+n,m,k = map(int, input().split())
+
+data = list(map(int, input().split()))
+data.sort()
+
+tmp = m // (k+1)
+remain = m%(k+1)
+
+ans = data[-1]*tmp*k + data[-2]*tmp + data[-1]*remain
+print(ans)
---
+++ 3-Greedy/3-3_NumberCardGame.py
@@ -0,0 +1,7 @@
+n,m = map(int, input().split())
+
+maxMin=0
+for i in range(n):
+    data = min(list(map(int, input().split())))
+    maxMin = max(maxMin,data)
+print(maxMin)

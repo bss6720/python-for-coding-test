763463df53a91f55f0b385654ad0c161d7e33dd4
Sungsu Bae <22442453+sungsubae@users.noreply.github.com>
2022-01-05 00:13:31 +0900

    Typo 'i' instead of 'j' when adding and removing key from padded

--- 4-Implementation/7-2_LockAndKey.py
+++ 4-Implementation/7-2_LockAndKey.py
@@ -1,3 +1,4 @@
+# Turns Matrix 90 degree clockwise
 def turn_list(l):
     length = len(l)
     turned = [[0] * length for _ in range(length)]
@@ -7,6 +8,7 @@ def turn_list(l):
     return turned
 
 
+# Check lock part and returns Boolean
 def check_open(key_lock):
     lock_len = len(key_lock) // 3
 
@@ -17,9 +19,17 @@ def check_open(key_lock):
     return True
 
 
+# For debugging
+def mat_print(matrix):
+    for row in matrix:
+        print(row)
+    print()
+
+
 def solution(key, lock):
     m = len(key)
     n = len(lock)
+    # Create padded list
     padded = [[0] * (3 * n) for _ in range(3 * n)]
 
     # place lock inside padding
@@ -31,14 +41,22 @@ def solution(key, lock):
         key = turn_list(key)
         for i in range(2 * n):
             for j in range(2 * n):
+                # Add Key to padded Matrix
                 for x in range(m):
                     for y in range(m):
-                        padded[i + x][i + y] += key[x][y]
+                        # Made Stupid Mistake at j + y, used i instead of j
+                        padded[i + x][j + y] += key[x][y]
                 if check_open(padded):
                     return True
-                print(padded)
+                # Remove Key from padded Matrix
                 for x in range(m):
                     for y in range(m):
-                        padded[i + x][i + y] -= key[x][y]
+                        padded[i + x][j + y] -= key[x][y]
 
     return False
+
+
+# Example Input
+ex_key = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
+ex_lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
+print(solution(ex_key, ex_lock))

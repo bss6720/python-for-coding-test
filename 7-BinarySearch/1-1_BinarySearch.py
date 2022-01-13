ae079dab2a83dcf2caae88ae3315644d6c37a955
Sungsu Bae <22442453+sungsubae@users.noreply.github.com>
2022-01-13 23:30:50 +0900

    Add Iterative Binary Search

--- 7-BinarySearch/1-1_BinarySearch.py
+++ 7-BinarySearch/1-1_BinarySearch.py
@@ -10,10 +10,24 @@ def binary_search_recur(array, target, start, end):
         return mid
 
 
+def binary_search_iter(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if target > array[mid]:
+            start = mid + 1
+        elif target < array[mid]:
+            end = mid - 1
+        else:
+            return mid
+    return None
+
+
 n, target = list(map(int, input().split()))
 
 array = list(map(int, input().split()))
 
 result = binary_search_recur(array, target, 0, n - 1)
+print('Recursive Binary Search: ', result)
 
-print(result)
+result = binary_search_iter(array, target, 0, n - 1)
+print('Iterative Binary Search: ', result)

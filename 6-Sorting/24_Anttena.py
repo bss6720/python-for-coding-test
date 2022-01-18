c9b18abbb654f00f51d77d5f319c651b915b9577
Sungsu Bae <22442453+sungsubae@users.noreply.github.com>
2022-01-19 00:03:40 +0900

    Sorting practical problem

--- 6-Sorting/24_Anttena.py
+++ 6-Sorting/24_Anttena.py
@@ -5,17 +5,4 @@ n = int(input())
 houses = list(map(int, sys.stdin.readline().split()))
 houses.sort()
 
-left = 0
-right = n
-cost = sum(houses)
-min_cost = cost
-pos = houses[0]
-for i in range(n):
-    cost += left*houses[i]
-    cost -= right*houses[i]
-    if cost < min_cost:
-        min_cost = cost
-        pos = houses[i]
-    left += 1
-    right -= 1
-print(pos)
+print(houses[(n-1)//2])

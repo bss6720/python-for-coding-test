15f81740da3bab17fe0473acb6706170bd2df228
Sungsu Bae <22442453+sungsubae@users.noreply.github.com>
2022-01-24 00:09:29 +0900

    Sorting Practical

--- 6-Sorting/25_FailureRate.py
+++ 6-Sorting/25_FailureRate.py
@@ -1,16 +1,23 @@
+# Programmers 30/lessons/42889
 def solution(N, stages):
-    answer = [0 for i in range(N+1)]
+    arr = [0 for i in range(N)]
+    answer = []
     for i in stages:
-        answer[i - 1] += 1
+        if i == N + 1:
+            continue
+        arr[i - 1] += 1
     count = len(stages)
-    arr = []
     for i in range(N):
         if count == 0:
-            continue
-        answer[i], count = answer[i] / count, count - answer[i]
-        arr.append((i, answer[i]))
-    sorted(arr, key=lambda t: t[1], reverse=True)
-    answer = [i[0] for i in arr]
+            answer.append((i, 0))
+        else:
+            answer.append((i, arr[i] / count))
+        count -= arr[i]
+
+    answer = sorted(answer, key=lambda t: t[1], reverse=True)
+    answer = [i[0]+1 for i in answer]
 
     return answer
-print(solution(5,[2, 1, 2, 6, 2, 4, 3, 3]))
+
+
+print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
